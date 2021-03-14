5cf17d0a46e17eb4d19968a106c2aeec502f9ef6
Matek <matek@puchatek.com>
2021-03-14 21:07:04 +0100

    day 1 commit 1

---
+++ players.js
@@ -0,0 +1,155 @@
+ export class Players{
+    constructor(){
+        this.players = [
+            {
+                name: 'Mateusz Andrzejewski',
+                position: 'A',
+                skillRate: '4'
+            },
+            {
+                name: 'Łukasz Błaszczuk',
+                position: 'D',
+                skillRate: '2'
+            },
+            {
+                name: 'Przemysław Celadyn',
+                position: 'G',
+                skillRate: '4'
+            },
+            {
+                name: 'Dawid Czekała',
+                position: 'A',
+                skillRate: '2'
+            },
+            {
+                name: 'Dawid Dąbrowski',
+                position: 'D',
+                skillRate: '3'
+            },
+            {
+                name: 'Łukasz Dąbrowski',
+                position: 'D',
+                skillRate: '3'
+            },
+            {
+                name: 'Carmine de Lucia',
+                position: 'D',
+                skillRate: '4'
+            },
+            {
+                name: 'Michał Jabłoński',
+                position: 'A',
+                skillRate: '3'
+            },
+            {
+                name: 'Mateusz Jeż',
+                position: '1',
+                skillRate: 'A'
+            },
+            {
+                name: 'Grzegorz Jóźwik',
+                position: 'D',
+                skillRate: '2'
+            },
+            {
+                name: 'Szymon Kaczyński',
+                position: 'A',
+                skillRate: '3'
+            },
+            {
+                name: 'Paweł Karczewski',
+                position: 'A',
+                skillRate: '1'
+            },
+            {
+                name: 'Radosław Kosiński',
+                position: 'D',
+                skillRate: '3'
+            },
+            {
+                name: 'Adam Kowalski',
+                position: 'D',
+                skillRate: '3'
+            },
+            {
+                name: 'Arkadiusz Krawczyk',
+                position: '2',
+                skillRate: 'D'
+            },
+            {
+                name: 'Karol Krzyżankiewicz',
+                position: 'D',
+                skillRate: '2'
+            },
+            {
+                name: 'Piotr Kuźniewicz',
+                position: 'D',
+                skillRate: '2'
+            },
+            {
+                name: 'Michał Kuźniewicz',
+                position: 'A',
+                skillRate: '1'
+            },
+            {
+                name: 'Kamil Limok',
+                position: 'D',
+                skillRate: '2'
+            },
+            {
+                name: 'Mateusz Malik',
+                position: 'A',
+                skillRate: '4'
+            },
+            {
+                name: 'Marcin Markowski',
+                position: 'A',
+                skillRate: '3'
+            },
+            {
+                name: 'Tomasz Nowok',
+                position: 'G',
+                skillRate: '4'
+            },
+            {
+                name: 'Jakub Piechocki',
+                position: 'D',
+                skillRate: '2'
+            },
+            {
+                name: 'Dawid Ptak',
+                position: 'A',
+                skillRate: '4'
+            },
+            {
+                name: 'Bartek Sadło',
+                position: 'A',
+                skillRate: '2'
+            },
+            {
+                name: 'Mateusz Sochacki',
+                position: 'G',
+                skillRate: '4'
+            },
+            {
+                name: 'Wojciech Trojanowski',
+                position: 'A',
+                skillRate: '1'
+            },
+            {
+                name: 'Kamil Wawoczny',
+                position: 'D',
+                skillRate: '2'
+            },
+            {
+                name: 'Artur Zaczkowski',
+                position: 'A',
+                skillRate: '1'
+            },
+
+        ];    
+    }
+
+}
+const allplayers = new Players();
+
